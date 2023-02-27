d7d7b25a3ba66e6b3b9c263e05be52ec83e93db0
aistomin <andrej.istomin.ikeen@gmail.com>
2023-02-27 19:37:38 +0100

    Redux course. Lesson 5. Counter feature optimization.

--- redux/my-redux-app/src/components/Header.js
+++ redux/my-redux-app/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {getCount, increaseCount} from "../features/posts/postsSlice";
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const count = useSelector(getCount);
     return (
         <header className="Header">
             <h1>Redux Blog</h1>
@@ -11,6 +15,7 @@ const Header = () => {
                     <li><Link to="post">Post</Link></li>
                     <li><Link to="user">Users</Link></li>
                 </ul>
+                <button onClick={() => dispatch(increaseCount())}>{count}</button>
             </nav>
         </header>
     );
